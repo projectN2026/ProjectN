0084bd2043669ee898a620d03e4b6ea31622d7e6
LSM-Shamrock <lsm.shamrock@gmail.com>
2026-03-29 09:47:29 +0900

    이것저것

--- Assets/03.Scripts/03.Game/Game/Game.cs
+++ Assets/03.Scripts/03.Game/Game/Game.cs
@@ -6,4 +6,14 @@ public class Game : SingletonBehaviour<Game>
     public CameraController Camera { get; private set; }
     [field: SerializeField, DescendantField] 
     public ObjectController Object { get; private set; }
+
+    public float RemainingTime { get; private set; } = 60f;
+
+
+    protected override void Update()
+    {
+        base.Update();
+
+        RemainingTime -= Time.deltaTime;
+    }
 }
---
+++ Assets/03.Scripts/03.Game/UI/RemainingTimeUI.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class RemainingTimeUI : BaseBehaviour
+{
+    [SerializeField, DescendantField]
+    private TextMeshProUGUI _remainingTimeText;
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+
+        var time = (int)Game.Instance.RemainingTime;
+        var m = time / 60;
+        var s = time % 60;
+
+        _remainingTimeText.text = $"{m:D2}:{s:D2}";
+    }
+}
